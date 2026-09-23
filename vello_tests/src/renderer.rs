c6353bce130540d628fa11fecd7516ac7cb62189
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2026-09-23 08:41:58 +0200

    Streamline wasm visual tests

--- vello_tests/src/renderer.rs
+++ vello_tests/src/renderer.rs
@@ -802,7 +802,7 @@ impl Renderer for GpuRenderer {
         width: u16,
         height: u16,
         num_threads: u16,
-        level: Level,
+        _level: Level,
         _: RenderMode,
         use_depth_buffer: bool,
     ) -> Self {
@@ -811,10 +811,6 @@ impl Renderer for GpuRenderer {
 
         if num_threads != 0 {
             panic!("GPU renderer doesn't support multi-threading");
-        }
-
-        if !level.is_fallback() {
-            panic!("GPU renderer doesn't support SIMD");
         }
 
         let mut settings = GpuRenderSettings::default();
